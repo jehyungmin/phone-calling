91d64d400d46a3a884b8464650bd03da3bd98357
jehyungmin <wpaudrl@gmail.com>
2019-04-22 06:33:19 +0900

    Updates

---
+++ precache-manifest.c7bf66dc0b8f398d2bacd5b8fbf9880d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8871b377e4b0fcd4413e",
+    "url": "/phone-calling/static/css/main.576aea68.chunk.css"
+  },
+  {
+    "revision": "8871b377e4b0fcd4413e",
+    "url": "/phone-calling/static/js/main.136663b5.chunk.js"
+  },
+  {
+    "revision": "1664deca4e969f6ba8fd",
+    "url": "/phone-calling/static/js/runtime~main.fead678c.js"
+  },
+  {
+    "revision": "d82afacfb8046780bdff",
+    "url": "/phone-calling/static/js/2.c272f57a.chunk.js"
+  },
+  {
+    "revision": "6847fa39576d4c6afdcff14681888a52",
+    "url": "/phone-calling/index.html"
+  }
+];
